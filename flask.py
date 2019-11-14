0aa283bf4583c3bc356b20580041822752d04233
Shane Moran <shane906.sm@gmail.com>
2019-11-14 18:31:40 +0000

    Initial Flask Web App file

---
+++ flask.py
@@ -0,0 +1,10 @@
+# Adapted from: https://palletsprojects.com/p/flask/
+
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get("name", "World")
+    return f'Hello, {escape(name)}!'
